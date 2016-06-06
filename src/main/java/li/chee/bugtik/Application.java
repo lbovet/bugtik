dbdec5fdfa446ac3d24aaa5948d89f29af1c2f72
Laurent Bovet <laurent.bovet@windmaster.ch>
2016-06-07 00:08:14 +0200

    Expose severity name

--- src/main/java/li/chee/bugtik/Application.java
+++ src/main/java/li/chee/bugtik/Application.java
@@ -3,6 +3,9 @@ package li.chee.bugtik;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +14,8 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @Controller
-public class Application {
+@Configuration
+public class Application extends RepositoryRestConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
@@ -24,7 +28,7 @@ public class Application {
     private SeverityRepository severityRepository;
 
     @PostConstruct
-    public void init() {
+    public void createInitialData() {
         Severity severity = new Severity("critical", "red");
         severityRepository.save(severity);
         Ticket ticket = new Ticket("Write specs", severity);
@@ -39,6 +43,12 @@ public class Application {
     public String index() {
         return "index.html";
     }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Severity.class);
+    }
+
 }
 
 
