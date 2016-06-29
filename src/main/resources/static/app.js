df23e38c528c10ad7ece581caa6c021eb9633e56
Laurent Bovet <laurent.bovet@windmaster.ch>
2016-06-29 18:29:59 +0200

    Loads severity and color transitively

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -7,20 +7,36 @@ angular
     // models
     self.projects = [];  // left menu
     self.tickets = [];   // main pane
+    self.selectedItem = null;
 
-    // loads the project list
-    api.$bind('projects', this.projects).$load();
-
-    // selects a project in the list
-    this.selectProject = function(project) {
-      project.$load().then(function(project) {
-        // loads projects ticket
-        project.$bind('tickets', self.tickets).$load();
+    function loadTicketData() {
+      self.tickets.forEach(function(ticket){
+        console.log(ticket);
+        ticket.$load()
+          .then(function(ticket) {
+            return ticket.$bind('severity').$load();
+          })
+          .then(function(severity) {
+            return severity.$bind('color').$load();
+          })
       });
     }
 
-    // search all tickets belonging to 'me'
-    this.selectMyTickets = function() {
-      api.$bind('tickets/search/findByOwner', self.tickets).$load({ owner: 'me'});
+    // selects a project in the list
+    self.selectProject = function(project) {
+      self.selectedItem = project;
+      project.$bind('tickets', self.tickets).$load().then(loadTicketData);
     }
+
+    // search all tickets belonging to 'me'
+    self.selectMyTickets = function() {
+      self.selectedItem = "my-tickets";
+      var search = api.$bind('tickets/search/findByOwner', self.tickets);
+      search.$bind.self = api.$bind('tickets').$bind.self;
+      search.$load({ owner: 'me'}).then(loadTicketData);
+    }
+
+    // initialize
+    api.$bind('projects', self.projects).$load();
+    self.selectMyTickets();
   }]);
