c2c889a245001c8ddf445797d8a1c6b0e32e9087
Laurent Bovet <laurent.bovet@windmaster.ch>
2016-07-20 11:16:45 +0200

    Cleanup

--- src/main/java/li/chee/bugtik/SeverityWithColor.java
+++ src/main/java/li/chee/bugtik/SeverityWithColor.java
@@ -2,9 +2,6 @@ package li.chee.bugtik;
 
 import org.springframework.data.rest.core.config.Projection;
 
-/**
- * Created by shaman on 7/3/16.
- */
 @Projection(name="withColor", types={ Severity.class })
 public interface SeverityWithColor {
     public String getName();
--- src/main/java/li/chee/bugtik/TicketWithSeverity.java
+++ src/main/java/li/chee/bugtik/TicketWithSeverity.java
@@ -2,9 +2,6 @@ package li.chee.bugtik;
 
 import org.springframework.data.rest.core.config.Projection;
 
-/**
- * Created by shaman on 7/2/16.
- */
 @Projection(name="withSeverity", types={ Ticket.class })
 public interface TicketWithSeverity {
     public long getId();
